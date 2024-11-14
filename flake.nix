1f951f67a4583659ade62575600e2546e3ded30a
Rashid J. Almheiri <r.muhairi@pm.me>
2024-11-14 23:20:14 +0400

    ziguanas -> rustaceans
    ngmi at the rate we switching languages 😂

--- flake.nix
+++ flake.nix
@@ -1,29 +1,39 @@
 {
-  description = "dev71/yapper: An implementation of the Yap protocol in Zig.";
+  description = "dev71/yapper: An implementation of the Yap protocol.";
 
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
       perSystem = {
-        l,
         pkgs,
+        lib,
+        inputs',
         ...
-      }: {
+      }: let
+        toolchain = (inputs'.fenix.packages).fromToolchainFile {
+          file = ./rust-toolchain.toml;
+          sha256 = "yMuSb5eQPO/bHv+Bcf/US8LVMbf/G/0MSfiPwBhiPpk=";
+        };
+      in {
         devShells.default = pkgs.mkShell {
           packages =
-            l.attrValues {
-              inherit (pkgs) zig zls cargo cargo-zigbuild;
+            builtins.attrValues {
+              inherit toolchain;
             }
-            ++ l.optional pkgs.stdenv.isDarwin [pkgs.libiconv];
+            ++ lib.optionals pkgs.stdenv.isDarwin [pkgs.libiconv];
 
           BORINGSSL_LIB_DIR = "${pkgs.boringssl}/lib";
         };
-      };
-      imports = [{perSystem = {lib, ...}: {_module.args.l = lib // builtins;};}];
+      }; # perSystem
     };
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 }
